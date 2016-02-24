d4e40addabdbc7803ea8556ed96571bd5437200e
Vanessa Alvarez <vanessalvarez8a@gmail.com>
2016-02-24 22:13:31 +0000

    some html fixed

--- js/app.js
+++ js/app.js
@@ -1,4 +1,25 @@
-var app = angular.module('blocChat', ['ui.router']);
+var app = angular.module('blocChat', []);
 
+ app.controller("chatController", function($scope) {
+     $scope.currentUser = "John"
+     $scope.chatRooms = ['Social Media Lovers', 'Health Starts Here', 'Developers']
+     $scope.chatters = [{name:'Ricardo Garcia', onlineStatus: true}, {name:'Manuel Martinez', onlineStatus: false}]
+     $scope.newRoom = function() {
+         alert('new Room click');
+     }
+     
+     $scope.messages = [{name:'Ricardo Garcia', text:'Hello'}]
+     
+     $scope.addMessage = function() {
+       $scope.messages.push({name:$scope.currentUser, text:$scope.currentMessage }) 
+     }
+     
+     $scope.createRoom = function() {
+      $scope.chatRooms.push($scope.newChannel)
+      $scope.showCreate = false;
+     }
+     
+
+ }) 
  
-
+ 
